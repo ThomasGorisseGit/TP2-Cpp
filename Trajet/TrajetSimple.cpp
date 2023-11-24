f3186aaef7287f3d7779a03c2b9a44abc734d718
charline <charline.roubaud@insa-lyon.fr>
2023-11-24 16:55:10 +0100

    Ajout des class

---
+++ Trajet/TrajetSimple.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+#include "TrajetSimple.h"
+
+
+TrajetSimple & TrajetSimple::operator = ( const TrajetSimple & unTrajetSimple )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+TrajetSimple::TrajetSimple ( const TrajetSimple & unTrajetSimple )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <TrajetSimple>" << endl;
+#endif
+} //----- Fin de TrajetSimple (constructeur de copie)
+
+
+TrajetSimple::TrajetSimple ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <TrajetSimple>" << endl;
+#endif
+} //----- Fin de TrajetSimple
+
+
+TrajetSimple::~TrajetSimple ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <TrajetSimple>" << endl;
+#endif
+} 
+
---
+++ Trajet/TrajetSimple.h
@@ -0,0 +1,17 @@
+#include "Trajet.h"
+#if ! defined ( TrajetSimple_H )
+#define TrajetSimple_H
+
+
+
+class TrajetSimple : public Trajet 
+{
+
+public:
+    TrajetSimple (char * depart, char * arrivee );
+    virtual ~TrajetSimple ( );
+
+
+};
+
+#endif // TrajetSimple_H
