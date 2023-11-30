1c33c29c1c106c00a227dea566aad277ce2c1088
Thomas <thomas.gorisse07@gmail.com>
2023-11-30 22:48:16 +0100

    Default makefile

--- Catalogue/Catalogue.cpp
+++ Catalogue/Catalogue.cpp
@@ -34,12 +34,12 @@ Catalogue::Catalogue ( const Catalogue & unCatalogue )
     cout << "Appel au constructeur de copie de <Catalogue>" << endl;
 #endif
     delete [] this->listeTrajet;
-    this->listeTrajet = malloc(sizeof(Trajet) * unCatalogue.listeTrajet->GetTaille());
+    this->listeTrajet = new Liste<Trajet>;
 
-    for(int i = 0 ; i<unCatalogue.listeTrajet.GetTaille(); i ++){
+    for(int i = 0 ; i<unCatalogue.listeTrajet->GetTaille(); i ++){
         this->listeTrajet->Ajouter(
-                unCatalogue.listeTrajet->GetValeur(i);
-                )
+                unCatalogue.listeTrajet->GetValeur(i)
+                );
     }
 } //----- Fin de Catalogue (constructeur de copie)
 
@@ -61,7 +61,7 @@ Catalogue::~Catalogue ( )
 } //----- Fin de ~Catalogue
 
 void Catalogue::Ajouter(const Trajet & trajet){
-    this->listeTrajet.Ajouter(trajet);
+    this->listeTrajet->Ajouter(trajet);
 }
 
 void Catalogue::Afficher() {
@@ -71,8 +71,8 @@ void Catalogue::Afficher() {
     cout << "---------------------------CATALOGUE-----------------------------" <<endl;
     cout << "-----------------------------------------------------------------" <<endl;
     int i =0;
-    while( i<this->listeTrajet.GetTaille() ){
-        this->listeTrajet.GetValeur(i).Afficher();
+    while( i<this->listeTrajet->GetTaille() ){
+        this->listeTrajet->GetValeur(i).Afficher();
     }
 }
 //------------------------------------------------------------------ PRIVE
