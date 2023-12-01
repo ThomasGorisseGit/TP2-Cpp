6a60be0cf219148210ad2b4d0b7407e683b785c5
ThomasGorisseGit <thomas.gorisse07@gmail.com>
2023-12-01 18:40:23 +0100

    Ajout de la recherche complexe

--- Catalogue/Catalogue.cpp
+++ Catalogue/Catalogue.cpp
@@ -58,7 +58,7 @@ void Catalogue::Afficher()
 
 void  Catalogue::Rechercher(const char* Depart, const char* Arrivee, Liste<Trajet> & ListeARemplir) const
 {
-    unsigned int taille = listeTrajet->GetTaille();
+    unsigned int taille = listeTrajet->GetTaopille();
     for (unsigned int i = 0; i < taille-1; i ++)
     {
         if(listeTrajet->GetValeur(i)->GetDepart() == Depart && listeTrajet->GetValeur(i)->GetArrivee() == Arrivee)
@@ -69,13 +69,39 @@ void  Catalogue::Rechercher(const char* Depart, const char* Arrivee, Liste<Traje
 
 }
 
+Liste<Trajet> * Catalogue::RechercheDepuisDepart(const char * depart){
+    Liste<Trajet> * listeARemplir = new Liste<Trajet>;
+    unsigned int taille = listeTrajet->GetTaille();
+    for (unsigned int i = 0; i < taille; i ++)
+    {
+        if(listeTrajet->GetValeur(i)->GetDepart() == depart)
+        {
+            listeARemplir->Ajouter(listeTrajet->GetValeur(i));
+        }
+    }
+    return listeARemplir;
+}
+
 // Fonction de recherche de trajet
 void RechercherTrajet(const char * depart, const char * arrivee) {
     cout << "Recherche de trajet de " << depart << " à " << arrivee << " :\n";
-
+}
     
-    
+void Catalogue::initSearch(const char * depart){
+    Liste<Trajet> * maliste = RechercheDepuisDepart(depart);
+    parse(maliste);
 
-
+    maliste->Afficher();
+}
+void Catalogue::parse(Liste<Trajet> * l){
+    for (int i =0;i<l->GetTaille();i++){
+        if (l->GetValeur(i)->GetArrivee() == arrivee){
+            l->GetValeur(i)->Afficher();
+        }
+        else{
+            Liste<Trajet> * l2 = RechercheDepuisDepart(l->GetValeur(i)->GetArrivee());
+            parse(l2);
+        }
+    }
 }
 
