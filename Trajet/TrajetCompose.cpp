6722740213a141f134d2fa466014c47841e0cab7
Gorisse Thomas <tgorisse@if501-219-09.insa-lyon.fr>
2023-12-01 16:31:11 +0100

    resolve

--- Trajet/TrajetCompose.cpp
+++ Trajet/TrajetCompose.cpp
@@ -6,16 +6,18 @@ using namespace std;
 #include "Trajet.h"
 #include "../Liste/Liste.h"
 
+
+
 void TrajetCompose::Afficher() const
 {
-    for (unsigned int i; i < *taille; i++)
+    for(unsigned int i; i < *taille; i++)
     {
         ListeTrajetCompose.GetValeur(i)->AfficherPetit();
     }
 }
 
-TrajetCompose::TrajetCompose(const Liste<TrajetSimple> &listeTrajetSimple) : Trajet(listeTrajetSimple.GetValeur(0)->GetDepart(), listeTrajetSimple.GetValeur(listeTrajetSimple.GetTaille() - 1)->GetArrivee()),
-                                                                             ListeTrajetCompose(listeTrajetSimple)
+TrajetCompose::TrajetCompose(const Liste<TrajetSimple> & listeTrajetSimple) : Trajet(listeTrajetSimple.GetValeur(0)->GetDepart(), listeTrajetSimple.GetValeur(listeTrajetSimple.GetTaille() - 1)->GetArrivee()),
+ListeTrajetCompose(listeTrajetSimple)
 {
 #ifdef MAP
     cout << "Appel au constructeur de <TrajetCompose>" << endl;
@@ -27,8 +29,8 @@ TrajetCompose::TrajetCompose(const Liste<TrajetSimple> &listeTrajetSimple) : Tra
 
 TrajetCompose::~TrajetCompose()
 {
-#ifdef MAP
-    cout << "Appel au destructeur de <TrajetCompose>" << endl;
-#endif
+    #ifdef MAP
+        cout << "Appel au destructeur de <TrajetCompose>" << endl;
+    #endif
     delete taille;
 }
