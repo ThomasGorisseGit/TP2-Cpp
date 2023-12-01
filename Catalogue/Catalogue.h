1a3e89a042d19d8565a56eb9fd4583c6efc6de82
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-12-01 16:47:49 +0100

    fix conflixt

--- Catalogue/Catalogue.h
+++ Catalogue/Catalogue.h
@@ -2,7 +2,6 @@
 #define Catalogue_H
 #include "../Trajet/Trajet.h"
 #include "../Liste/Liste.h"
-<<<<<<< HEAD
 #include "../Trajet/TrajetSimple.h"
 #include "../Trajet/TrajetCompose.h"
 //--------------------------------------------------- Interfaces utilisées
@@ -16,8 +15,6 @@
 //
 //
 //------------------------------------------------------------------------
-=======
->>>>>>> 91c14e20bdc62b2444f5dcab9ffbf26958b2f14a
 
 class Catalogue
 {
@@ -34,13 +31,12 @@ public:
     //      Appel de la méthode afficher catalogue.Afficher()
 
 <<<<<<< HEAD
-    void Rechercher(const char* Depart, const char* Arrivee, Liste<Trajet> & ListeARemplir) const;
-    //Fonction qui recherche un trajet dans le catalogue et renvoie une liste des trajets ayant les
-    //meme départ et arrivee
+    void Rechercher(const char *Depart, const char *Arrivee, Liste<Trajet> &ListeARemplir) const;
+    // Fonction qui recherche un trajet dans le catalogue et renvoie une liste des trajets ayant les
+    // meme départ et arrivee
 
-
-//------------------------------------------------- Surcharge d'opérateurs
-    Catalogue & operator = ( const Catalogue & unCatalogue );
+    //------------------------------------------------- Surcharge d'opérateurs
+    Catalogue &operator=(const Catalogue &unCatalogue);
 =======
     //------------------------------------------------- Surcharge d'opérateurs
     Catalogue &operator=(const Catalogue &unCatalogue);
