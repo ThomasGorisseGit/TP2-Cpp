5d40d88a92efd63920e0092eca4ad66723eabd48
Thomas <thomas.gorisse07@gmail.com>
2023-12-04 09:25:47 +0100

    test

--- Liste/Liste.h
+++ Liste/Liste.h
@@ -27,11 +27,7 @@ public:
      *
      * @param index L'index du maillon dont on veut la valeur
      */
-<<<<<<< HEAD
-    T *GetValeur(unsigned int index) const;
-=======
     T* GetValeur(unsigned int index) const;
->>>>>>> main
 
     /**
      * @brief Retourne la taille de la liste
@@ -46,11 +42,8 @@ public:
      * @param valeur Le Trajet à ajouter à la liste
      */
     void Ajouter(T* valeur);
-<<<<<<< HEAD
 
     void Ajouter(Liste<T> * liste);
-=======
->>>>>>> main
 
     /**
      * @brief Permet de rechercher un Trajet dans la liste
@@ -71,10 +64,6 @@ protected:
     Maillon<T> *tete;
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 template <typename T>
 Liste<T>::Liste()
 {
@@ -93,11 +82,7 @@ Liste<T>::~Liste()
     cout << "Appel au destructeur de <Liste>" << endl;
 #endif
     // On parcourt la liste et on supprime chaque maillon
-<<<<<<< HEAD
     // TODO : vérifier que ça marche
-=======
-    //TODO : vérifier que ça marche
->>>>>>> main
     Maillon<T> *courrent = tete;
     while (courrent != nullptr)
     {
@@ -110,11 +95,7 @@ Liste<T>::~Liste()
 }
 
 template <typename T>
-<<<<<<< HEAD
-T *Liste<T>::GetValeur(unsigned int index) const
-=======
 T* Liste<T>::GetValeur(unsigned int index) const
->>>>>>> main
 {
     Maillon<T> *courrent = tete;
     unsigned int i = 0;
@@ -160,7 +141,6 @@ void Liste<T>::Ajouter(T* valeur)
 }
 
 template <typename T>
-<<<<<<< HEAD
 void Liste<T>::Ajouter(Liste<T> * liste){
     for (unsigned int i = 0; i < liste->GetTaille(); i++)
     {
@@ -169,8 +149,6 @@ void Liste<T>::Ajouter(Liste<T> * liste){
 }
 
 template <typename T>
-=======
->>>>>>> main
 bool Liste<T>::Rechercher(const T *valeur) const
 {
     Maillon<T> *courrent = tete;
@@ -191,26 +169,9 @@ void Liste<T>::Afficher() const
     Maillon<T> *courrent = tete;
     while (courrent != nullptr)
     {
-<<<<<<< HEAD
-        // FIXME courrent->afficher(); // Ici, on suppose que la classe T possède une méthode afficher()
-=======
         //FIXME courrent->afficher(); // Ici, on suppose que la classe T possède une méthode afficher()
->>>>>>> main
         courrent = courrent->getSuivant();
     }
 }
 
-<<<<<<< HEAD
 #endif // LISTE_H
-=======
-
-
-
-
-
-
-#endif // LISTE_H
-
-
-
->>>>>>> main
