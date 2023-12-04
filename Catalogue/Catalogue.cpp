7523f94a0f331f9b99ab0049610b1f5cdb11a522
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-12-04 09:03:23 +0100

    reformatage

--- Catalogue/Catalogue.cpp
+++ Catalogue/Catalogue.cpp
@@ -15,8 +15,7 @@ Catalogue::Catalogue(const Catalogue &unCatalogue)
     for (unsigned int i = 0; i < unCatalogue.listeTrajet->GetTaille(); i++)
     {
         this->listeTrajet->Ajouter(
-            unCatalogue.listeTrajet->GetValeur(i)
-            );
+            unCatalogue.listeTrajet->GetValeur(i));
     }
 } //----- Fin de Catalogue (constructeur de copie)
 
@@ -35,7 +34,7 @@ Catalogue::~Catalogue()
 #endif
 } //----- Fin de ~Catalogue
 
-void Catalogue::Ajouter(Trajet* trajet)
+void Catalogue::Ajouter(Trajet *trajet)
 {
     this->listeTrajet->Ajouter(trajet);
 }
@@ -56,7 +55,7 @@ void Catalogue::Afficher()
         i++;
     }
 }
-void Catalogue::Afficherl(Liste <Trajet> * liste)
+void Catalogue::Afficherl(Liste<Trajet> *liste)
 {
     unsigned int i = 0;
     while (i < liste->GetTaille())
@@ -66,88 +65,82 @@ void Catalogue::Afficherl(Liste <Trajet> * liste)
     }
 }
 
-void  Catalogue::Rechercher(const char* Depart, const char* Arrivee, Liste<Trajet> & ListeARemplir) const
+void Catalogue::Rechercher(const char *Depart, const char *Arrivee, Liste<Trajet> &ListeARemplir) const
 {
     unsigned int taille = listeTrajet->GetTaille();
-    for (unsigned int i = 0; i < taille-1; i ++)
+    for (unsigned int i = 0; i < taille - 1; i++)
     {
-        //TODO : Tester et voir si le egal marche comparé aux strcmp
-        if(strcmp(listeTrajet->GetValeur(i)->GetDepart(), Depart) == 0 && strcmp(listeTrajet->GetValeur(i)->GetArrivee(), Arrivee) == 0)
+        // TODO : Tester et voir si le egal marche comparé aux strcmp
+        if (strcmp(listeTrajet->GetValeur(i)->GetDepart(), Depart) == 0 && strcmp(listeTrajet->GetValeur(i)->GetArrivee(), Arrivee) == 0)
         {
             ListeARemplir.Ajouter(listeTrajet->GetValeur(i));
         }
-        
     }
-
 }
-
 
 // Fonction de recherche de trajet
-void RechercherTrajet(const char * depart, const char * arrivee) {
+void RechercherTrajet(const char *depart, const char *arrivee)
+{
     cout << "Recherche de trajet de " << depart << " à " << arrivee << " :\n";
 }
-    
-void Catalogue::initSearch(const char * depart,const char * arrivee){
-    
-    Liste<Trajet> * listeTrajetParcourus = RechercheDepuisDepart(depart);
-    Liste<Trajet> * listeARemplir = new Liste<Trajet>;
-    
-    //TODO : Split trajet compose en trajet simple
-    
+
+void Catalogue::initSearch(const char *depart, const char *arrivee)
+{
+
+    Liste<Trajet> *listeTrajetParcourus = RechercheDepuisDepart(depart);
+    Liste<Trajet> *listeARemplir = new Liste<Trajet>;
+
+    // TODO : Split trajet compose en trajet simple
+
     this->TrajetArrivantADestination(listeTrajetParcourus, arrivee, listeARemplir);
 
-    //Reverse listeARemplir : 
-    Liste<Trajet> * listeARemplirReverse = new Liste<Trajet>;
+    // Reverse listeARemplir :
+    Liste<Trajet> *listeARemplirReverse = new Liste<Trajet>;
     unsigned int taille = listeARemplir->GetTaille();
-    for (unsigned int i = 0; i < taille; i ++)
+    for (unsigned int i = 0; i < taille; i++)
     {
-        listeARemplirReverse->Ajouter(listeARemplir->GetValeur(taille-i-1));
+        listeARemplirReverse->Ajouter(listeARemplir->GetValeur(taille - i - 1));
     }
     this->Afficherl(listeARemplirReverse);
     // TODO : delete listeARemplirReverse
     // TODO : delete listeARemplir
     // TODO : delete listeTrajetParcourus
-    /*
-        delete listeARemplirReverse;
-        delete listeARemplir;
-        delete listeTrajetParcourus;
-
-        // Throw Segmentation error : On call of Trajet Destructor and Maillon Destructor.
-    */
-
 }
-bool Catalogue::TrajetArrivantADestination(Liste<Trajet> * listeTrajetParcourus, const char * arrivee,Liste<Trajet> * listeARemplir){
+
+bool Catalogue::TrajetArrivantADestination(Liste<Trajet> *listeTrajetParcourus, const char *arrivee, Liste<Trajet> *listeARemplir)
+{
     unsigned int taille = listeTrajetParcourus->GetTaille();
-    for (unsigned int i = 0; i < taille; i ++)
+    for (unsigned int i = 0; i < taille; i++)
     {
-        if (strcmp(listeTrajetParcourus->GetValeur(i)->GetArrivee(), arrivee) == 0 )
+        if (strcmp(listeTrajetParcourus->GetValeur(i)->GetArrivee(), arrivee) == 0)
         {
             listeARemplir->Ajouter(listeTrajetParcourus->GetValeur(i));
-            //cout << listeTrajetParcourus->GetValeur(i)->GetDepart() << " -> " << listeTrajetParcourus->GetValeur(i)->GetArrivee() << endl;
-            //cout << "--------------------"<<endl;
+            // cout << listeTrajetParcourus->GetValeur(i)->GetDepart() << " -> " << listeTrajetParcourus->GetValeur(i)->GetArrivee() << endl;
+            // cout << "--------------------"<<endl;
             return true;
-        }else{
-            Liste<Trajet> * listeTrajetParcourus2 = RechercheDepuisDepart(listeTrajetParcourus->GetValeur(i)->GetArrivee());
+        }
+        else
+        {
+            Liste<Trajet> *listeTrajetParcourus2 = RechercheDepuisDepart(listeTrajetParcourus->GetValeur(i)->GetArrivee());
             if (TrajetArrivantADestination(listeTrajetParcourus2, arrivee, listeARemplir))
             {
                 listeARemplir->Ajouter(listeTrajetParcourus->GetValeur(i));
-                //cout << listeTrajetParcourus->GetValeur(i)->GetDepart() << " -> " << listeTrajetParcourus->GetValeur(i)->GetArrivee() << endl;
-
+                // cout << listeTrajetParcourus->GetValeur(i)->GetDepart() << " -> " << listeTrajetParcourus->GetValeur(i)->GetArrivee() << endl;
             }
         }
     }
     return false;
 }
-Liste<Trajet> * Catalogue::RechercheDepuisDepart(const char * depart){
-    Liste<Trajet> * listeARemplir = new Liste<Trajet>;
+Liste<Trajet> *Catalogue::RechercheDepuisDepart(const char *depart)
+{
+    Liste<Trajet> *listeARemplir = new Liste<Trajet>;
     unsigned int taille = listeTrajet->GetTaille();
-    for (unsigned int i = 0; i < taille; i ++)
+    for (unsigned int i = 0; i < taille; i++)
     {
-        if (strcmp(listeTrajet->GetValeur(i)->GetDepart(), depart) == 0 )
+        if (strcmp(listeTrajet->GetValeur(i)->GetDepart(), depart) == 0)
         {
             listeARemplir->Ajouter(listeTrajet->GetValeur(i));
         }
     }
     return listeARemplir;
 }
-
