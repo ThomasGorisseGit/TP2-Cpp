37acf149af24ef559e190077e9a73b73260710d1
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-12-08 22:22:29 +0100

    ajout de commentaires

--- Trajet/TrajetCompose.cpp
+++ Trajet/TrajetCompose.cpp
@@ -6,42 +6,37 @@ using namespace std;
 #include "Trajet.h"
 #include "../Liste/Liste.h"
 
-
-
 void TrajetCompose::Afficher() const
 {
     cout << "Affiche du compose" << endl;
-    for(unsigned int i=0; i < *taille; i++)
+    for (unsigned int i = 0; i < *taille; i++)
     {
         listeTrajetCompose->GetValeur(i)->AfficherPetit();
         cout << "Affichage composé" << endl;
     }
 }
 
-TrajetCompose::TrajetCompose(Liste<TrajetSimple>  listeTrajetSimple) : Trajet(listeTrajetSimple.GetValeur(0)->GetDepart(), listeTrajetSimple.GetValeur(listeTrajetSimple.GetTaille() - 1)->GetArrivee())
+TrajetCompose::TrajetCompose(Liste<TrajetSimple> listeTrajetSimple) : Trajet(listeTrajetSimple.GetValeur(0)->GetDepart(), listeTrajetSimple.GetValeur(listeTrajetSimple.GetTaille() - 1)->GetArrivee())
 {
 #ifdef MAP
     cout << "Appel au constructeur de <TrajetCompose>" << endl;
 #endif
-    taille = new unsigned int;
-    *taille = listeTrajetSimple.GetTaille();
+    taille = new unsigned int;               // On alloue la mémoire pour la taille
+    *taille = listeTrajetSimple.GetTaille(); // On récupère la taille de la liste de trajet simple
 
-    this->listeTrajetCompose = new Liste<TrajetSimple>;
+    this->listeTrajetCompose = new Liste<TrajetSimple>; // On alloue la mémoire pour la liste de trajet simple
 
-     for (unsigned int i = 0; i < *taille; i++)
+    for (unsigned int i = 0; i < *taille; i++)
     {
         listeTrajetCompose->Ajouter(listeTrajetSimple.GetValeur(i));
     }
-
-    
-    // création d'un objet Trajet
 }
 
 TrajetCompose::~TrajetCompose()
 {
-    #ifdef MAP
-        cout << "Appel au destructeur de <TrajetCompose>" << endl;
-    #endif
+#ifdef MAP
+    cout << "Appel au destructeur de <TrajetCompose>" << endl;
+#endif
     delete taille;
     delete listeTrajetCompose;
 }
