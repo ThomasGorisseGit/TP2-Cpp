30df6d2447d14409a0c450ba84f79299804aa419
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-12-09 10:21:34 +0100

    little changes

--- Trajet/TrajetCompose.h
+++ Trajet/TrajetCompose.h
@@ -5,29 +5,27 @@
 #include "Trajet.h"
 #include "TrajetSimple.h"
 
-
 class TrajetCompose : public Trajet
 {
 
 public:
-    TrajetCompose(Liste<TrajetSimple>  & listeTrajetSimple);
+    TrajetCompose(Liste<TrajetSimple> &listeTrajetSimple);
 
-    //void Ajouter(const TrajetSimple & trajet);
+    // void Ajouter(const TrajetSimple & trajet);
 
-    void Decompose(Liste<TrajetSimple> & listeTrajetSimple);
+    void Decompose(Liste<TrajetSimple> &listeTrajetSimple);
 
     void Afficher() const;
 
-
     virtual int GetType() const;
     virtual unsigned int GetTailleTrajet() const;
-    Trajet * GetTrajetSimple(unsigned int indice) const;
+    Trajet *GetTrajetSimple(unsigned int indice) const;
 
     virtual ~TrajetCompose();
 
 protected:
-    Liste<TrajetSimple> * listeTrajetCompose;
-    unsigned int * taille;
+    Liste<TrajetSimple> *listeTrajetCompose;
+    unsigned int *taille;
 };
 
 #endif
